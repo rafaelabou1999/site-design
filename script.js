c70cbeec061b96db1dec9e4db6a2ec71fae905b3
Rafaela Bourdette <120579642+rafaelabou1999@users.noreply.github.com>
2023-05-23 12:18:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+$(".header-arrow").click(function () {
+  $("html, body").animate({ scrollTop: 770 }, "30");
+});
+
+$(window).on("scroll", function () {
+  var windowTop = $(this).scrollTop();
+
+  $(this).addClass("header-active");
+});
+
+$(window).scroll(function () {
+  var windowTop = $(this).scrollTop();
+
+  $("section").each(function () {
+    if (windowTop > $(this).offset().top - 1200) {
+      $(this).addClass("part");
+    } else {
+      $(this).removeClass("part");
+    }
+  });
+});
+
+$(".brand").on("click", function () {
+  $(window).scrollTop(0);
+});
+
+$(".home").on("click", function (e) {
+  e.preventDefault;
+  $(window).scrollTop(0);
+});
+
+$("nav a").on("click", function () {
+  var itemId = $(this).attr("href");
+
+  $("nav a").removeClass("active");
+  $(this).addClass("active");
+  $(itemId).addClass("active");
+});
